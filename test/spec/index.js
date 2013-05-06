c4d7dab918bdfe63eac8a75a85ea7daac309633c
Chris Saunders - Home Laptop <slapheadted@gmail.com>
2013-05-06 11:55:57 +0100

    Removal of bootstrap specs

--- test/spec/index.js
+++ test/spec/index.js
@@ -1,14 +1,7 @@
 define(function() {
   return {
     specs: [
-      'spec/models/SongSpec',
-      'spec/models/PlayerSpec',
-      'spec/collections/SongsSpec',
-      'spec/views/PlayerSpec',
-      'spec/views/HomeSpec',
-      'spec/models/SearchSpec',
-      'spec/views/SearchSpec',
-      '../scripts/components/loader/test/spec/views/LoadingSpec'
+      'spec/views/LoadingSpec'
     ]
   };
 });
